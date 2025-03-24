d62b7dc5e8f362cc24dc716e8461cc5a27fc562d
sebastianogiannitti <sebastiano.giannitti@studio.unibo.it>
2025-03-24 17:39:53 +0100

    plugins

--- conwayguialone/src/main/java/unibo/disi/conwayguialone/ws/WSConwayguiLifeMqtt.java
+++ conwayguialone/src/main/java/unibo/disi/conwayguialone/ws/WSConwayguiLifeMqtt.java
@@ -15,7 +15,8 @@ import unibo.basicomm23.utils.CommUtils;
  */
 
 //@Component //Ci pensa WebSocketConfiguration
-@ServerEndpoint("/wsupdates")
+@ServerEndpoint("/wsupdates") // spring istanzia questa classe nel
+//momento in cui parte
 public class WSConwayguiLifeMqtt extends AbstractWebSocketHandler{   
 	
 	private static WSConwayguiLifeMqtt myself        = null;
@@ -27,7 +28,7 @@ public class WSConwayguiLifeMqtt extends AbstractWebSocketHandler{
 	private WebSocketSession owner;
 	
 	
-	
+	//creo un singleton, da riutilizzare per le chiamate successive
 	public static WSConwayguiLifeMqtt getInstance() {
 		//CommUtils.outblue( name + " | getInstance " + myself );
 		if (myself == null) {
@@ -40,25 +41,32 @@ public class WSConwayguiLifeMqtt extends AbstractWebSocketHandler{
 		/*
 		 * Stesso schema di connessione usato da Life
 		 */
+		//appena si avvia usa l'interazione mqtt
  		useMqttInteraction();
     }
 	
 	protected void useMqttInteraction() {
 		CommUtils.outblue(name + " | useMqttInteraction  "  );
+		//controlla se nelle variabili c'è una certa variabile
 		String broker = System.getenv("MQTTBROKER_URL");
+		//nel mio caso non c'è quindi il broker è sicuramente null
+		//quindi assumo che il broker sia locale
 		if( broker == null ) broker = "tcp://localhost:1883";
 		else {
 			//CommUtils.outgreen( "WSConwaygui | useMqttInteraction in docker to " + broker ); 	
 		}					
 
-		
+
+		//capito dove si trova il broker creo l'interazione
 		mqttConn = new MqttInteraction(  name, broker, "guiin", "lifein" );
- 		
+
+		//nuovo thread
 		new Thread() {
 			public void run() { //activateReceive(); NECESSARIO SE Life non usa WS
 				try {
 					while (true) {
 	 					String msg = "";
+						 //si mette in attesa BLOCCANTE dei messaggi che il servizio manda alla GUI
 						msg = mqttConn.receiveMsg();
 //						CommUtils.outblack(name + " | WSConwayguiLifeMqtt receives via mqtt: " + msg);
 					    //if( msg.startsWith("cell-") ) broadcastToWebSocket(msg);
@@ -105,6 +113,7 @@ public class WSConwayguiLifeMqtt extends AbstractWebSocketHandler{
 	 * Gestione comandi provenienti dalla GUI
 	 */ 	
 	@Override //since AbstractWebSocketHandler
+	//prendo il messaggio che arriva
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 // 		CommUtils.outgreen(name + " | receives: " + message + " wirh ownerOn=" + ownerOn );		
 		String cmd = message.getPayload();
@@ -112,14 +121,20 @@ public class WSConwayguiLifeMqtt extends AbstractWebSocketHandler{
 	}
 	
 	protected void elabMsgMqtt( String cmd ) throws Exception {
+		//se il messaggio inizia con "cell(" lo mando attraverso la websocket a tutti i client collegati
 		if( cmd.startsWith("cell(") ){ //IL comando arriva dalla applicazione remota
 			broadcastToWebSocket(cmd);
 			return;
-	    }		
+	    }
+
+		//prendo i messaggi che arrivano dall'applicazione e li inoltro ai client connessi
 		if( cmd.startsWith("GAME") ){ //IL comando arriva dalla applicazione remota
 			broadcastToWebSocket(cmd);
 			return;
-	    }		
+	    }
+
+		//clear significa che lato server vuole azzerare tutte le celle,
+		//anche qui manda il messaggio a tutti i browser collegati
 		if( cmd.startsWith("clear") ){ //IL comando arriva dalla GUI ma ottimizzo
 			CommUtils.outmagenta(name + " | receives: " + cmd   );	
 			broadcastToWebSocket(cmd);
@@ -127,7 +142,6 @@ public class WSConwayguiLifeMqtt extends AbstractWebSocketHandler{
 			return;
 	    }		
  	}
-
 
 	protected void elabMsg(String cmd, WebSocketSession session) throws Exception {
 		if( cmd.startsWith("cell(") ){ //IL comnado arriva dalla applicazione remota
